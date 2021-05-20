c2d03aa18094ae66c88de6cdde7f7bbd689cbdcf
Caroline Schmid <caroline.schmid@hotmail.fr>
2021-05-20 23:27:03 +0200

    fonctions dans bons fichiers d'entêtes

--- GraphReseau.h
+++ GraphReseau.h
@@ -45,23 +45,3 @@ typedef tabSommetsGR liste_graph_reseau[N];
 /**********************************************************************
  * PROTOTYPES DES FONCTIONS (TYPE ABSTRAIT)
  **********************************************************************/
-
-/* 
- * Intialiser le graph réseau à partir du fichier DIMACS
- * @param1 : le fichier DIMACS
- * @param2 : un tableau de liste chainée de dimension N (qui sera redimensionner à la fin de lecture du fichier DIMACS)
- * @param3 : on récupere la source du graph de réseau, on utilise un int car le graph est numéroté (ref: HYOPTHESE)
- * @param4 : on récupere la fin du graph de réseau, on utiliser un int car le graph est numéroté (ref: HYPOTHESE)
- * @param5 : on récupere le nombre de sommets 
- * le param3, param4 et param5 sont récupérer pour etre utiliser dans la fonction principale de dinic 
- */
-
-extern void buildGraph(FILE fichierDimacs, tabSommetsGR T, int* source, int* sink, int* n);
-
-/*
- * Résultat final: le résult final sera rendu par une façon modifié de fichier DIIMACS (cf: Rapport d'analyse Partie Génération du fichier résultat)
- * @param1 : un fichier type File est pris en paramtere sur lequel les résultats seront stockées 
- * @param2 : tabSommetsGR (un graph du réseau)
- */
-
-extern void renderResult(FILE result, tabSommetsGR T);
