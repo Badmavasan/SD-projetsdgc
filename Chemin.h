df4102727f8cc565274358c322aabd28861c4bc2
Caroline Schmid <caroline.schmid.etu@univ-lille.fr>
2021-05-20 11:03:49 +0200

    idée de structure de données, à faire valider - confirmer

--- Chemin.h
+++ Chemin.h
@@ -2,3 +2,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+struct maillon_chemin {
+    int id;
+    int flot;
+    struct maillon_chemin * next;
+};
+
+
+struct liste_chemin {
+    int id;
+    struct maillon_chemin * head;
+};
