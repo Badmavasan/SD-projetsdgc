1601aad50969a0fb71a07b1f0b5f391dbeb51594
badmavasan.kirouchenassamy.etu <badmavasan.kirouchenassamy.etu@univ-lille.fr>
2021-05-25 12:44:22 +0200

    working files

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 int main(void) {
     printf("Saisissez le nom du fichier contenant le réseau initial.");
     char fichier_dinic [50];
-//     scanf("%s", fichier_dinic);
-    strcpy(fichier_dinic, "net1.txt");
+    scanf("%s", fichier_dinic);
+    // strcpy(fichier_dinic, "net5.txt");
     int source, sink, nb_sommets,nb_arcs;
 
     struct liste_graph_reseau* tabReseau = buildGraph(fichier_dinic, &source, &sink, &nb_sommets, &nb_arcs);
@@ -15,7 +15,7 @@ int main(void) {
     dinic(tabReseau, &f, source, sink,nb_sommets);
     printf("Le flot est passé de 0 à %d\n", f);
     renderResult("result.txt",tabReseau,nb_sommets,nb_arcs,source,sink,f);
-    
+
     clear_liste_graph_reseau(tabReseau,nb_sommets);
     return 0;
 }
